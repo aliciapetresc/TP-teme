c3da1c1fecdae4382fe8c3039ba0cfb74109aaf1
aliciapetresc <61683098+aliciapetresc@users.noreply.github.com>
2020-03-20 14:40:39 +0200

    Lab 4- ex 3

---
+++ L4.c
@@ -0,0 +1,19 @@
+// P3: Sa se realizeze un program care interschimba 2 valori folosind un pointer ca variabila auxiliara.
+
+#include <stdio.h>
+#include <stdio.h>
+
+int main()
+{
+	int a, b, *n;
+	//allocam memorie
+	n = (int *)malloc(sizeof(int));
+	printf("Dati o valoare variabilei a: "); scanf("%d", &a);
+	printf("Dati o valoare variabilei b: "); scanf("%d", &b);
+	//interschimbarea
+	*n = a;
+	a = b;
+	b = *n;
+	printf("a devine %d ; b devine %d\n", a, b);
+	return 0;
+}
