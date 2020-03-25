01dc4621050d48cc92f438c84ef57adb837240d8
aliciapetresc <61683098+aliciapetresc@users.noreply.github.com>
2020-03-25 21:45:59 +0200

    Update P3.c

--- L3/P3.c
+++ L3/P3.c
@@ -6,14 +6,19 @@
 int main()
 {
 	int a, b, *n;
+	
 	//allocam memorie
 	n = (int *)malloc(sizeof(int));
 	printf("Dati o valoare variabilei a: "); scanf("%d", &a);
 	printf("Dati o valoare variabilei b: "); scanf("%d", &b);
+	
 	//interschimbarea
 	*n = a;
 	a = b;
 	b = *n;
+	
 	printf("a devine %d ; b devine %d\n", a, b);
+	
+	system("pause");
 	return 0;
 }
